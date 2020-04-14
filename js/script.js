7a39c5a6b355f6531e374190db2e384bf25eeffc
Nischal Rai <nischalpilmong@yahoo.com>
2020-04-14 13:58:41 +0200

    Adding a book through form and displayed in the UI

--- js/script.js
+++ js/script.js
@@ -26,14 +26,28 @@ class UI{
     static addBookToList(book){
        const list = document.querySelector('#book-list');
        const row = document.createElement('tr');
-       row.innerHTML = ` 
-                        <td>${book.title}</td> 
+       row.innerHTML = `<td>${book.title}</td> 
                          <td>${book.author}</td>
                          <td>${book.isbn}</td>
-                         <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
-                         `;
+                         <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>`;
        list.appendChild(row);                  
     }
     
 }
+//Display Books
 document.addEventListener('DOMContentLoaded',UI.displayBooks);
+
+//Add a Book
+document.querySelector('#book-form').addEventListener('submit',(e) => {
+    //prevent from actual submit
+    e.preventDefault();
+    //Get the Form values
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const isbn = document.querySelector('#isbn').value;
+    //Instantiate Book
+    const bookObj = new Book(title, author, isbn);
+    //Add Book to UI
+    UI.addBookToList(bookObj);
+
+});
